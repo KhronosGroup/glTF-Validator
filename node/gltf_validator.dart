2b452d88bfecb36f12e1c23365ca8451f20ec879
Alexey Knyazev <lexa.knyazev@gmail.com>
2020-01-14 15:49:07 +0400

    Revert redundant check

--- node/gltf_validator.dart
+++ node/gltf_validator.dart
@@ -177,7 +177,7 @@ Future<Map<String, Object>> _validateResourcesAndGetReport(
     final loader =
         _getResourcesLoader(context, result, externalResourceFunction);
     await loader.load(
-        validateAccessorData: options?.validateAccessorData ?? true);
+        validateAccessorData: options.validateAccessorData ?? true);
   }
   return ValidationResult(uri, context, result,
           writeTimestamp: options?.writeTimestamp ?? true)
