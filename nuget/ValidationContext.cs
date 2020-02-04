bcd3a630f7811da50fbdccb63a683c0c39c8b42f
vpenades <megaz0ne23@hotmail.com>
2020-02-05 00:10:43 +0100

    Nuget package, first commit

---
+++ nuget/ValidationContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace glTF2Validator
+{
+    /// <summary>
+    /// Represents the context used to validate files
+    /// </summary>
+    public class ValidationContext
+    {
+        /// <summary>
+        /// Path to the gltf-validator command line executable (platform specific).
+        /// </summary>
+        public static string ValidatorExePath { get; set; }
+
+        /// <summary>
+        /// Timeout to cancel the validation operation.
+        /// </summary>
+        public static TimeSpan TimeOut { get; set; } = TimeSpan.FromSeconds(10);
+        
+        static ValidationContext()
+        {
+            SetDefaultPath();
+        }
+
+        
+        private static void SetDefaultPath()
+        {
+            var assemblyDir = System.IO.Path.GetDirectoryName(typeof(ValidationContext).Assembly.Location);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (RuntimeInformation.OSArchitecture != Architecture.X64) return;
+                ValidatorExePath = System.IO.Path.Combine(assemblyDir, "gltf_validator.exe");
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                ValidatorExePath = System.IO.Path.Combine(assemblyDir, "gltf_validator");
+            }
+        }
+        
+        /// <summary>
+        /// Validates a glTF file.
+        /// </summary>
+        /// <param name="gltfFilePath">Path to the glTF file.</param>
+        /// <returns>A report.</returns>
+        public static ValidationReport ValidateFile(string gltfFilePath)
+        {
+            if (string.IsNullOrWhiteSpace(ValidatorExePath)) return null;
+
+            if (!System.IO.File.Exists(ValidatorExePath)) throw new System.IO.FileNotFoundException(ValidatorExePath);
+
+            if (!System.IO.Path.IsPathRooted(gltfFilePath)) gltfFilePath = System.IO.Path.GetFullPath(gltfFilePath);
+
+            var psi = new System.Diagnostics.ProcessStartInfo(ValidatorExePath);
+            psi.Arguments = $"-p -r -a \"{gltfFilePath}\"";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardError = true;
+
+            var p = System.Diagnostics.Process.Start(psi);
+
+            if (!p.WaitForExit(TimeOut.Milliseconds))
+            {
+                try { p.Kill(); } catch { }
+            }
+
+            var rawReport = p.StandardError.ReadToEnd();
+
+            if (string.IsNullOrWhiteSpace(rawReport)) return null;
+
+            return new ValidationReport(gltfFilePath, rawReport);
+        }
+    }
+}
---
+++ nuget/ValidationResult.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace glTF2Validator
+{
+    /// <summary>
+    /// Represents the resulting report of a glTF validation.
+    /// </summary>
+    public sealed class ValidationReport
+    {
+        internal ValidationReport(string srcPath, string rawReport)
+        {
+            var lines = rawReport.Split('\n');
+
+            var status = 0;
+
+            var www = new List<string>();
+            var eee = new List<string>();
+
+            foreach (var l in lines)
+            {
+                if (l == "\tWarnings:") { status = 1; continue; }
+                if (l == "\tErrors:") { status = 2; continue; }
+
+                if (status == 1) www.Add(l.Trim());
+                if (status == 2) eee.Add(l.Trim());
+            }
+
+            FilePath = srcPath;
+            RawReport = rawReport;
+            Warnings = www;
+            Errors = eee;
+        }
+
+        /// <summary>
+        /// Gets the file path from which this report was originated.
+        /// </summary>
+        public string FilePath { get; private set; }
+
+        /// <summary>
+        /// Gets the original report as emitted by gltf-validator.
+        /// </summary>
+        public string RawReport { get; private set; }
+
+        /// <summary>
+        /// Gets the warnings as a list.
+        /// </summary>
+        public IReadOnlyList<String> Warnings { get; private set; }
+
+        /// <summary>
+        /// Gets the errors as a list.
+        /// </summary>
+        public IReadOnlyList<String> Errors { get; private set; }
+
+        
+        /// <summary>
+        /// True if warnings were found.
+        /// </summary>
+        public bool HasWarnings => Warnings.Count > 0;
+
+        /// <summary>
+        /// True if errors were found.
+        /// </summary>
+        public bool HasErrors => Errors.Count > 0;
+
+        public override string ToString()
+        {
+            return RawReport;
+        }
+    }
+}
