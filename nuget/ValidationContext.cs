deb68c4ff755349c731b401004b3661e63566a29
Vicente Penades Armengot <5433822+vpenades@users.noreply.github.com>
2020-02-06 16:32:09 +0100

    Added nuget project prototype.

---
+++ nuget/ValidationContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace glTF2Validator
+{
+    /// <summary>
+    /// Represents the context used to validate files
+    /// </summary>
+    public class ValidationContext
+    {
+        /// <summary>
+        /// Path to the gltf-validator command line executable (platform specific).
+        /// </summary>
+        public static string ValidatorExePath { get; set; }
+
+        /// <summary>
+        /// Timeout to cancel the validation operation.
+        /// </summary>
+        public static TimeSpan TimeOut { get; set; } = TimeSpan.FromSeconds(10);
+        
+        static ValidationContext()
+        {
+            SetDefaultPath();
+        }
+
+        
+        private static void SetDefaultPath()
+        {
+            var assemblyDir = System.IO.Path.GetDirectoryName(typeof(ValidationContext).Assembly.Location);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                if (RuntimeInformation.OSArchitecture != Architecture.X64) return;
+                ValidatorExePath = System.IO.Path.Combine(assemblyDir, "gltf_validator.exe");
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                ValidatorExePath = System.IO.Path.Combine(assemblyDir, "gltf_validator");
+            }
+        }
+        
+        /// <summary>
+        /// Validates a glTF file.
+        /// </summary>
+        /// <param name="gltfFilePath">Path to the glTF file.</param>
+        /// <returns>A report.</returns>
+        public static ValidationReport ValidateFile(string gltfFilePath)
+        {
+            if (!System.IO.File.Exists(gltfFilePath)) throw new ArgumentException($"File {gltfFilePath} not found.", nameof(gltfFilePath));
+
+            if (string.IsNullOrWhiteSpace(ValidatorExePath)) return null;
+
+            if (!System.IO.File.Exists(ValidatorExePath)) throw new System.IO.FileNotFoundException(ValidatorExePath);
+
+            if (!System.IO.Path.IsPathRooted(gltfFilePath)) gltfFilePath = System.IO.Path.GetFullPath(gltfFilePath);
+
+            var psi = new System.Diagnostics.ProcessStartInfo(ValidatorExePath);
+            psi.Arguments = $"-p -r -a -o \"{gltfFilePath}\"";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            using (var p = System.Diagnostics.Process.Start(psi))
+            {
+                if (!p.WaitForExit(TimeOut.Milliseconds))
+                {
+                    try { p.Kill(); } catch { }
+                }
+
+                var jsonReport = p.StandardOutput.ReadToEnd();
+
+                if (string.IsNullOrWhiteSpace(jsonReport)) return null;
+
+                return ValidationReport.Parse(jsonReport);
+            }
+        }
+    }
+}
---
+++ nuget/ValidationResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace glTF2Validator
+{
+    /// <summary>
+    /// Represents the resulting report of a glTF validation.
+    /// </summary>
+    public sealed class ValidationReport
+    {
+        public static ValidationReport Parse(string json)
+        {
+            return JsonSerializer.Deserialize<ValidationReport>(json);
+        }
+
+        public string uri { get; set; }
+        public string mimeType { get; set; }
+        public string validatorVersion { get; set; }
+
+        public ValidationIssues issues { get; set; }
+
+        public ValidationInfo info { get; set; }
+    }
+
+    public sealed class ValidationIssues
+    {
+        public int numErrors { get; set; }
+        public int numWarnings { get; set; }
+        public int numInfos { get; set; }
+        public int numHints { get; set; }
+        public string[] messages { get; set; }
+        public bool truncated { get; set; }
+    }
+
+    public sealed class ValidationInfo
+    {
+        public string version { get; set; }
+        public string generator { get; set; }
+        public int animationCount { get; set; }
+        public int materialCount { get; set; }
+        public bool hasMorphTargets { get; set; }
+        public bool hasSkins { get; set; }
+        public bool hasTextures { get; set; }
+        public bool hasDefaultScene { get; set; }
+        public int drawCallCount { get; set; }
+        public int totalTriangleCount { get; set; }
+        public int maxUVs { get; set; }
+        public int maxInfluences { get; set; }
+        public int maxAttributes { get; set; }
+    }
+}
