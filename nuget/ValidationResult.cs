eddf81c1fae4feece52b09ebefd4b21e8382dc55
vpenades <5433822+vpenades@users.noreply.github.com>
2020-02-12 23:38:13 +0100

    Adjusted JSon parser to allow properties to have proper c# case.

--- nuget/ValidationResult.cs
+++ nuget/ValidationResult.cs
@@ -11,50 +11,59 @@ namespace glTF2Validator
     /// </summary>
     public sealed class ValidationReport
     {
-        public static ValidationReport Parse(string json)
+        public static ValidationReport Load(string filePath)
         {
-            return JsonSerializer.Deserialize<ValidationReport>(json);
+            var json = System.IO.File.ReadAllText(filePath);
+            return Parse(json);
         }
 
-        public string uri { get; set; }
-        public string mimeType { get; set; }
-        public string validatorVersion { get; set; }
-        public ValidationIssues issues { get; set; }
-        public ValidationInfo info { get; set; }
+        public static ValidationReport Parse(string json)
+        {
+            var options = new JsonSerializerOptions();
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+            return JsonSerializer.Deserialize<ValidationReport>(json, options);
+        }
+
+        public string Uri { get; set; }
+        public string MimeType { get; set; }
+        public string ValidatorVersion { get; set; }
+        public ValidationIssues Issues { get; set; }
+        public ValidationInfo Info { get; set; }
     }
 
     public sealed class ValidationIssues
     {
-        public int numErrors { get; set; }
-        public int numWarnings { get; set; }
-        public int numInfos { get; set; }
-        public int numHints { get; set; }
-        public ValidationMessage[] messages { get; set; }
-        public bool truncated { get; set; }
+        public int NumErrors { get; set; }
+        public int NumWarnings { get; set; }
+        public int NumInfos { get; set; }
+        public int NumHints { get; set; }
+        public ValidationMessage[] Messages { get; set; }
+        public bool Truncated { get; set; }
     }
 
     public sealed class ValidationMessage
     {
-        public string code { get; set; }
-        public string message { get; set; }
-        public int severity { get; set; }
-        public string pointer { get; set; }
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public int Severity { get; set; }
+        public string Pointer { get; set; }
     }
 
     public sealed class ValidationInfo
     {
-        public string version { get; set; }
-        public string generator { get; set; }
-        public int animationCount { get; set; }
-        public int materialCount { get; set; }
-        public bool hasMorphTargets { get; set; }
-        public bool hasSkins { get; set; }
-        public bool hasTextures { get; set; }
-        public bool hasDefaultScene { get; set; }
-        public int drawCallCount { get; set; }
-        public int totalTriangleCount { get; set; }
-        public int maxUVs { get; set; }
-        public int maxInfluences { get; set; }
-        public int maxAttributes { get; set; }
+        public string Version { get; set; }
+        public string Generator { get; set; }
+        public int AnimationCount { get; set; }
+        public int MaterialCount { get; set; }
+        public bool HasMorphTargets { get; set; }
+        public bool HasSkins { get; set; }
+        public bool HasTextures { get; set; }
+        public bool HasDefaultScene { get; set; }
+        public int DrawCallCount { get; set; }
+        public int TotalTriangleCount { get; set; }
+        public int MaxUVs { get; set; }
+        public int MaxInfluences { get; set; }
+        public int MaxAttributes { get; set; }
     }
 }
